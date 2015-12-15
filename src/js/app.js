03089db6da06b1be93e18e782c008848116b4833
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-15 04:00:03 -0700

    back to the drawing board

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
   console.log('hello werld');
 
   var encodedUrlForApiCall = xmlgetter.getEncodedUrl("https://en.wikipedia.org/w/api.php?action=feedrecentchanges");
-  var myApiCall = "http://rss2json.com/api.json?&rss_url="+encodedUrlForApiCall
+  var myApiCall = "https://rss2json.com/api.json?&rss_url="+encodedUrlForApiCall
+
 
   $.get(myApiCall, function(data){
     console.log(data["feed"]["title"]);
