c6e73fca6ad9055aff6107ec367e4adf3886f943
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-17 07:49:36 -0700

    moar tests

--- src/js/spec/arraybuddy_spec.js
+++ src/js/spec/arraybuddy_spec.js
@@ -21,9 +21,32 @@ describe("flatten", function(){
   })
 })
 
-describe('chaining', function(){
-  it("should be chainable", function(){
-    
-    expect(ab.chunk([1,2,3,4],2).flatten()).toEqual([1,2,3,4])
+describe('a use case', function(){
+  it("should be wrap chunks and then flatten them", function(){
+    function chunkAndJoin(arr, len){
+      var myarray = ab.chunk(arr, len)
+      myarray = myarray.map(function(x){
+        return x.join('')
+      })
+      myarray = ab.chunk(myarray,1)
+      return myarray
+    }
+
+    var testarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    testarray = chunkAndJoin(testarray, 3)
+    expect(testarray).toEqual([['123'], ['456'], ['789']])
+
+    testarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    testarray = chunkAndJoin(testarray, 2)
+    expect(testarray).toEqual([['12'], ['34'], ['56'], ['78'], ['9']])
+
+    testarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    testarray = ab.chunk(testarray, 3)
+    testarray = testarray.map(function(a){
+      return a.join('')
+    }).map(function(s){
+      return "<div>"+s+"</div>"
+    })
+    expect(testarray).toEqual(['<div>123</div>', '<div>456</div>', '<div>789</div>'])
   })
 })
