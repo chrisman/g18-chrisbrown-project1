b6bd9a994e55b6007edd6b9b3822b97b932366df
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-17 14:50:53 -0700

    stylish

--- src/js/app.js
+++ src/js/app.js
@@ -14,25 +14,25 @@ function makeCard(o) {
     .filter(excludeMarkup)
     .join('')
 
-  var $test = $(desc)
-  console.log($test);
+  var $fullDescription = $($.parseHTML(desc))
+  var $briefSummary = $('<table>').append($fullDescription.find('ins, del').closest('tr'))
 
   return '\
   <div id="card-'+ idBase +'" class="container-fluid col-md-4 card card--dim">\
     <div class="row card__header">\
       <div class="col-md-12">\
-      <h3><a href="' + o["link"] + '">' + o["title"] + '</a><small> by ' + o["author"] + '</small></h3>\
+      <h3><a href="' + o["link"] + '">' + o["title"] + '</a><small> by <a href="https://en.wikipedia.org/wiki/User:' + o["author"] + '">'+ o["author"] +'</a></small></h3>\
       </div>\
     </div>\
     <div class="row card__summary">\
       <div class="col-md-12">\
-      ' + desc + '\
+      ' + $('<table>').append($briefSummary).html() + '\
       </div>\
     </div>\
     <div class="row card__footer">\
       <div class="col-md-12 text-right">\
-      <span>Show summary</span>\
-      <button id="btn-'+ idBase +'" class="btn btn-default"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span><span>&nbsp;Thank</span></button>\
+      <a id="modal--show'+ idBase +'" class="btn btn-default">Show summary</a>\
+      <button id="btn-'+ idBase +'" class="btn btn-default"> <i class="fa fa-heart-o"></i><span>&nbsp;Thank</span></button>\
       </div>\
     </div>\
   </div>'
@@ -72,17 +72,27 @@ function handleGetMyApiCall(data) {
 
 
 $(document).ready(function() {
-
   var encodedUrlForApiCall = xmlgetter.getEncodedUrl(_str["getFeedRecentChanges"]);
 
   $.get(_str["rss2json"]+encodedUrlForApiCall, handleGetMyApiCall)
 
   $(document).on('click', 'button', function(e) {
+    var that = $(this)
+    that.html('<i class="fa fa-spinner fa-spin"></i>')
     console.log('click logged');
+
     $.get(_str["corsAnywhere"]+_str["getTokens"], function(res){
       _str["postOptions"]["token"] = res["query"]["tokens"]["csrftoken"]
-      $.post(_str["corsAnywhere"]+_str["apiEndPoint"], _str["postOptions"], function(res){
-        console.log(res);
+
+      $.post(_str["corsAnywhere"]+_str["apiEndPoint"], _str["postOptions"]).done(function(x){
+        if ('error' in x) {
+          that.toggleClass("btn-default").toggleClass("btn-danger").html("uh-oh..")
+        } else {
+          that.toggleClass("btn-default").toggleClass("btn-success").html('<i class="fa fa-heart fa-lg"></i>')
+        }
+        console.log(x);
+      }).fail(function(){
+        console.log("fail");
       })
     })
   })
